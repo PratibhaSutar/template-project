82a2bbccae5fdebef93bdaf16dedda320242cd47
Pratibha <pratibhasutar6451@gmail.com>
2022-07-29 01:02:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent //implements OnInit
+{
+  
   title = 'template-project';
+  months = ['Jan','Feb','March','April','May','June','July','Aug','Sept','Oct','Nov','Dec'];
+  enabled = false;
+  c = 1;
+
+  /*ngOnInit(): void {
+    setInterval(()=>{
+      this.enabled = !this.enabled;
+    }
+    ,1000);
+  }*/
+  clicked(){
+    this.enabled = !this.enabled;
+  }
 }
